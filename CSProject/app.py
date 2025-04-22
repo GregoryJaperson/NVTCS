274d040bfe7836dd9e595a7713693cfdbb4542d2
GregoryJaperson <89696741+GregoryJaperson@users.noreply.github.com>
2025-04-22 21:52:46 +0200

    Successfully implemented Flask to connect python and html

--- CSProject/app.py
+++ CSProject/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, render_template_string
 import Project
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return #render_template("index.html")
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
